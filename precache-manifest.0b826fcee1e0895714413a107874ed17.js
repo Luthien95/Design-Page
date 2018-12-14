e095ba186d76e7e7db2fbb6dad28adcf37170443
Aleksandra Salak <aleksandrasalak@gmail.com>
2018-12-14 19:35:21 +0100

    Updates

---
+++ precache-manifest.0b826fcee1e0895714413a107874ed17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "246318e48ba704bc8d74",
+    "url": "/Design-Page/static/js/runtime~main.246318e4.js"
+  },
+  {
+    "revision": "a2d89663dbabfbb5913b",
+    "url": "/Design-Page/static/js/main.a2d89663.chunk.js"
+  },
+  {
+    "revision": "ca0b152d65d15978242a",
+    "url": "/Design-Page/static/js/1.ca0b152d.chunk.js"
+  },
+  {
+    "revision": "a2d89663dbabfbb5913b",
+    "url": "/Design-Page/static/css/main.66ef010d.chunk.css"
+  },
+  {
+    "revision": "4287c84ca7575d3ce3c4fc3465cf5b2a",
+    "url": "/Design-Page/index.html"
+  }
+];
